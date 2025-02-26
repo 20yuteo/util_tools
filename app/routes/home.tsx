9ca671ac3cad62ba9a1e70822ab11d32a1ccb90b
20yuteo <20yuteo@gmail.com>
2025-02-27 01:42:52 +0900

    Add Google Generative AI integration with Gemini model

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import { RepositoryProvider } from "~/adapter/repositories/provider";
+import { GoogleGenerativeAI } from "@google/generative-ai"
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,6 +11,11 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader() {
+  const googleGenerativeAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_API_KEY);
+  const model = googleGenerativeAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+  const response = await model.generateContent("who are you?");
+  console.log(response.response.text());
+
   const tweetRepository = new RepositoryProvider().tweet;
   await tweetRepository.postTweet('Hello, world!');
 }
