d13bd9df98e3f0631c715e8f50bc787c086815da
20yuteo <20yuteo@gmail.com>
2025-02-28 00:25:20 +0900

    Remove loader function from home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,22 +9,6 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export async function loader() {
-  const slackRepository = new RepositoryProvider().slack;
-  try {
-    const llmRepository = new RepositoryProvider().llm;
-    const tweetRepository = new RepositoryProvider().tweet;
-    const content = await llmRepository.generateContent('who are you?');
-    await tweetRepository.postTweet(content);
-
-    await slackRepository.postMessage('Success: ' + content);
-  } catch (error) {
-    await slackRepository.postMessage('Error: ' + error);
-  }
-
-
-}
-
 export default function Home() {
   return <Welcome />;
 }
