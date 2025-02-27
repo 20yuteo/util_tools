d13f37349eda55fa5c9df36e2055cc742794728d
20yuteo <20yuteo@gmail.com>
2025-02-28 00:22:20 +0900

    Implement environment-based repository initialization

--- app/adapter/repositories/provider.ts
+++ app/adapter/repositories/provider.ts
@@ -4,6 +4,9 @@ import type { TweetRepository } from "~/domain/repositories/tweet";
 import { LLMRepositoryEmpty } from "~/test/domain/repositories/llm";
 import { SlackRepositoryEmpty } from "~/test/domain/repositories/slack";
 import { TweetRepositoryEmpty } from "~/test/domain/repositories/tweet";
+import { TweetRepositoryImpl } from "./tweet";
+import { LLMRepositoryImpl } from "./llm";
+import { SlackRepositoryImpl } from "./slack";
 
 export class RepositoryProvider {
   tweet: TweetRepository;
@@ -11,8 +14,14 @@ export class RepositoryProvider {
   slack: SlackRepository;
 
   constructor() {
-    this.tweet = new TweetRepositoryEmpty();
-    this.llm = new LLMRepositoryEmpty();
-    this.slack = new SlackRepositoryEmpty();
+    if (process.env.ENV === 'production') {
+      this.tweet = new TweetRepositoryImpl();
+      this.llm = new LLMRepositoryImpl();
+      this.slack = new SlackRepositoryImpl();
+    } else {
+      this.tweet = new TweetRepositoryEmpty();
+      this.llm = new LLMRepositoryEmpty();
+      this.slack = new SlackRepositoryEmpty();
+    }
   }
 }
